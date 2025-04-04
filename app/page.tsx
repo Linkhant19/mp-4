dba8cd0ea672f69f9c5b7cc24f1fa71b365a7bea
Linkhant19 <lkk19@bu.edu>
2025-04-04 18:35:20 -0400

    finished mp-4

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center bg-red-300 p-2">
-            <h1>Hi! I am in CS391 lab.</h1>
-            <p>I am having so much fun!</p>
+    <div className="flex flex-col justify-center items-center bg-red-300 p-2 h-screen">
+            <h1 className="text-gray-900 text-7xl font-semibold">SUPERHEROES AND VILLAINS</h1>
+            <p className="text-gray-900 text-1xl font-semibold">all my favorites called from an api</p>
     </div>
   );
 }
